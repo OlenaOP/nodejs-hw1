91d169469538ec6a0d9cece235d24664a6dce99e
OlenaOP <elena.peredriy@gmail.com>
2023-12-04 00:50:21 +0200

    add tasks

---
+++ contacts.js
@@ -0,0 +1,40 @@
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const deleteContact = contacts[index];
+  contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deleteContact;
+}
+
+async function addContact(name, email, phone) {
+  // Повертає об'єкт доданого контакту.
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
